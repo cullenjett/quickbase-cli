11fd78cab69fa43d018e069ed97fe68fb0d2bbd3
cullenjett@gmail.com <cullenjett@gmail.com>
2016-08-29 16:13:45 -0600

    qb deploy <dir> sends the html file to quickbase. create an api client class to wrap api calls

--- bin/qb-deploy.js
+++ bin/qb-deploy.js
@@ -1,5 +1,46 @@
 #!/usr/bin/env node
 
+let ApiClient = require('../lib/api')
 let args = require('commander').parse(process.argv).args
+let fs = require('fs')
+let path = require('path')
 
-console.log(`you called 'qb deploy' with the following args: ${args}`)
+let sourceDirectory = args[0]
+let config = {
+  username: 'cullenjett',
+  realm: 'ais',
+  dbid: 'bjzrx8ciy'
+}
+let api = new ApiClient(config)
+
+const deploy = (sourceDir) => {
+  let isRelativePath = sourceDir.charAt(0) == '.'
+  let source
+
+  if (isRelativePath) {
+    source = path.join(process.cwd(), sourceDir)
+  } else {
+    source = sourceDir
+  }
+
+  let fileNames = fs.readdirSync(source)
+
+  let indexHtml = fileNames.find(fileName => path.extname(fileName) == '.html')
+  let mainJs = fileNames.find(fileName => path.extname(fileName) == '.js')
+  let mainCss = fileNames.find(fileName => path.extname(fileName) == '.css')
+
+  let jsFileName = path.basename(mainJs)
+  let cssFileName = path.basename(mainCss)
+
+  let htmlText = fs.readFileSync(path.join(sourceDir, indexHtml), 'utf-8')
+    .replace(new RegExp(jsFileName, 'g'), 'SUCCESS.JS')
+    .replace(new RegExp(cssFileName, 'g'), 'SUCCESS.CSS')
+
+  let pageName = 'quickbase-cli.html'
+  return api.uploadPage(htmlText, pageName).then(res => {
+    console.log('RES:', res)
+    console.log("DONE")
+  })
+}
+
+deploy(sourceDirectory)
